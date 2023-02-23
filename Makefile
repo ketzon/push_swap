316d309327e17a0479d1445eef0a4668719d6ca2
fbesson <fbesson@student.42.fr>
2023-02-23 14:28:05 +0100

    makefile working

--- Makefile
+++ Makefile
@@ -1,5 +1,31 @@
 NAME = push_swap
-CFLAGS = -g -Wall -Werror -Wextra
-PRINTF = ./printf/libftprintf.a
-LIBFT = ./libft/libft.a
-push_swap: $(PRINTF)
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g
+LIBFT_DIR = ./libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRC = push_swap.c
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+clean:
+	rm -f $(OBJ)
+	make -C $(LIBFT_DIR) clean
+
+fclean: clean
+	rm -f $(NAME)
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fbesson <fbesson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 12:43:46 by fbesson           #+#    #+#             */
-/*   Updated: 2023/02/22 15:37:42 by fbesson          ###   ########.fr       */
+/*   Updated: 2023/02/22 17:00:55 by fbesson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ int	main()
 {
 	ft_printf("test\n");
 	printf("test2\n");
+	char **res = ft_split("je suis florian", 'o');
+	int i = 0;
+	while (res[i])
+	{
+		printf("%s\n", res[i]);
+		i++;
+	}
+	printf("error\n");
+	return (0);
 }
-
-/* void	perror() */
-/* { */
-/* 	ft */
-/* } */
