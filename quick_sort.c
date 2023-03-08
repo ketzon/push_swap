346928ff0537dc69f1447b7aaa22f4a788e9650a
fbesson <fbesson@student.42.fr>
2023-03-08 15:13:42 +0100

    segfault fix

--- quick_sort.c
+++ quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: fbesson <fbesson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 17:52:01 by fbesson           #+#    #+#             */
-/*   Updated: 2023/03/08 14:41:43 by fbesson          ###   ########.fr       */
+/*   Updated: 2023/03/08 15:12:22 by fbesson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ int	ft_quick_sort(t_push *start, t_push *end)
 	if (pivot->prev != NULL)
 		ft_quick_sort(start, pivot->prev);
 	if (pivot->next != NULL)
-		ft_quick_sort(pivot->prev, end);
+		ft_quick_sort(pivot->next, end);
+	printf("hello\n");
 	return (0);
 }
